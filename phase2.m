5cafba8994d2bf3928d56cbe557a3efa0f58d190
Greg Schneider <gjschneider11@gmail.com>
2025-11-05 14:37:07 -0500

    Enhancements to plot

--- phase2.m
+++ phase2.m
@@ -1,3 +1,5 @@
+% Author: Greg Schneider, Abhay Ratti
+
 function plot_min_max_signals(signals, name)
     % Plot Signals of Lowest and Highest Frequency Bands
     figure('Name', name);
@@ -6,18 +8,30 @@ function plot_min_max_signals(signals, name)
     time1 = (0:length(signals{1})-1) / 16000;
     timeN = (0:length(signals{length(signals)})-1) / 16000;
     
+    % Work out Frequency range for both signals
+    n = length(signals);
+    low_freq = 100; % Hz
+    hi_freq = 8000; % Hz
+    freq_step = (hi_freq - low_freq) / n;
+
+    freq0 = [low_freq, low_freq + freq_step];
+    freqn = [low_freq + freq_step*(n-1), low_freq + freq_step*n];
+
     subplot(1,2,1);
     plot(time1, signals{1});
-    title('Lowest Frequency Band Signal');
+    title(sprintf('Lowest Frequency Band Signal (%.1f Hz - %.1f Hz)', freq0(1), freq0(2)));
     xlabel('Time (s)');
     ylabel('Amplitude');
     grid on;
     subplot(1,2,2);
     plot(timeN, signals{length(signals)});
-    title('Highest Frequency Band Signal');
+    title(sprintf('Highest Frequency Band Signal (%.1f Hz - %.1f Hz)', freqn(1), freqn(2)));
     xlabel('Time (s)');
     ylabel('Amplitude');
     grid on;
+    
+    % Add overall title to the figure
+    sgtitle(name);
 end
 
 function split_signals = split_frequency(n, signal)
